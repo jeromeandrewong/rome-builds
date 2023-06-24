93bdf144a23b683c14dea86c1f57ba837b650674
Jerome Wong <jeromewongjh@gmail.com>
2023-06-24 21:44:28 +0800

    refactor: logo and about page

--- src/config.ts
+++ src/config.ts
@@ -30,12 +30,7 @@ export const SOCIALS: SocialObjects = [
     linkTitle: `${SITE.title} on Facebook`,
     active: false,
   },
-  {
-    name: "Instagram",
-    href: "https://github.com/jeromeandrewong",
-    linkTitle: `${SITE.title} on Instagram`,
-    active: false,
-  },
+
   {
     name: "LinkedIn",
     href: "https://linkedin.com/in/jeromeandrewong",
@@ -53,35 +48,5 @@ export const SOCIALS: SocialObjects = [
     href: "https://twitter.com/jeromeandrewong",
     linkTitle: `${SITE.title} on Twitter`,
     active: true,
-  },
-  {
-    name: "Discord",
-    href: "https://twitter.com/jeromeandrewong",
-    linkTitle: `${SITE.title} on Discord`,
-    active: false,
-  },
-  {
-    name: "GitLab",
-    href: "https://twitter.com/jeromeandrewongr",
-    linkTitle: `${SITE.title} on GitLab`,
-    active: false,
-  },
-  {
-    name: "Reddit",
-    href: "https://twitter.com/jeromeandrewong",
-    linkTitle: `${SITE.title} on Reddit`,
-    active: false,
-  },
-  {
-    name: "Telegram",
-    href: "https://twitter.com/jeromeandrewong",
-    linkTitle: `${SITE.title} on Telegram`,
-    active: false,
-  },
-  {
-    name: "Mastodon",
-    href: "https://twitter.com/jeromeandrewong",
-    linkTitle: `${SITE.title} on Mastodon`,
-    active: false,
   },
 ];
--- src/pages/about.md
+++ src/pages/about.md
@@ -3,9 +3,19 @@ layout: ../layouts/AboutLayout.astro
 title: "About"
 ---
 
-RomeBuilds is a personal blog where I document my journey as an aspiring software developer. I am enthusiastic about web development and product-first engineering.
+<div class='flex flex-col sm:flex-row sm:gap-x-5'>
+
+<img src="/assets/logo1.png" class="w-1/3 rounded-md aspect-square object-cover" alt="cats">
+
+<div>
+
+RomeBuilds is a personal blog where I document my journey as an aspiring software developer.
 
 I am an Information Systems student, Coffee Snob, Cafe Hopper, amateur Poker Player and most importantly, a Cat Lover.
+
+</div>
+
+</div>
 
 <div class='flex gap-x-1'>
   <img src="/assets/cat1.png" class="w-1/3 rounded-md" alt="cats">
@@ -13,12 +23,8 @@ I am an Information Systems student, Coffee Snob, Cafe Hopper, amateur Poker Pla
   <img src="/assets/cat3.png" class="w-1/3 rounded-md" alt="cats">
 </div>
 
-## Current Favourite Tech Stack
+## Currently Interested in:
 
-[T3 Stack](https://create.t3.gg/):
-
-- Next.js
-- NextAuth.js / [Clerk](https://clerk.com/)
-- Prisma
-- tRPC
-- TailwindCSS / [shadcn/ui](https://ui.shadcn.com/)
+- Systems Design
+- Go
+- Low Latency / High Frequency Trading
